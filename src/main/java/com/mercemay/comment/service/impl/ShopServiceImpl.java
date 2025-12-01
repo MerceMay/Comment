2030117e191ef75b816fef916587010b0338996e
MerceMay <n15697952078@gmail.com>
2025-12-01 20:35:40 +0800

    Implementing nearby shops feature

--- src/main/java/com/mercemay/comment/service/impl/ShopServiceImpl.java
+++ src/main/java/com/mercemay/comment/service/impl/ShopServiceImpl.java
@@ -1,6 +1,8 @@
 package com.mercemay.comment.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.mercemay.comment.dto.Result;
 import com.mercemay.comment.entity.Shop;
@@ -8,13 +10,19 @@ import com.mercemay.comment.mapper.ShopMapper;
 import com.mercemay.comment.service.IShopService;
 import com.mercemay.comment.utils.CacheClient;
 import com.mercemay.comment.utils.RedisData;
+import com.mercemay.comment.utils.SystemConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.data.geo.GeoResults;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.domain.geo.GeoReference;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static com.mercemay.comment.utils.RedisConstants.*;
@@ -62,7 +70,50 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Result queryShopByType(Integer typeId, Integer current, Double x, Double y) {
-        return null;
+        if (x == null || y == null) {
+            // 不需要根据坐标查询，按数据库查询
+            Page<Shop> page = query()
+                    .eq("type_id", typeId)
+                    .page(new Page<>(current, SystemConstants.DEFAULT_PAGE_SIZE));
+            return Result.ok(page.getRecords());
+        }
+        // 计算开始分页位置和结束位置
+        int from = (current - 1) * SystemConstants.DEFAULT_PAGE_SIZE;
+        int end = current * SystemConstants.DEFAULT_PAGE_SIZE;
+
+        // 查询redis，按照距离排序，分页。结果：shopId、distance
+        String key = SHOP_GEO_KEY + typeId; // 键
+        GeoResults<RedisGeoCommands.GeoLocation<String>> results =
+                stringRedisTemplate.opsForGeo().search( // GEOSEARCH key BYLONLAT x y BYRADIUS 10 WITHDISTANCE
+                        key, // 键
+                        GeoReference.fromCoordinate(x, y), // 坐标
+                        new Distance(5000), // 半径5km
+                        RedisGeoCommands.GeoSearchCommandArgs.newGeoSearchArgs().includeDistance().limit(end) // 包含距离，分页
+                );
+        if (results == null) {
+            return Result.ok(Collections.emptyList());
+        }
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> list = results.getContent(); // 获取结果列表
+        if (list.size() <= from) {
+            // 没有下一页
+            return Result.ok(Collections.emptyList());
+        }
+        // 获取从from到end的部分
+        List<Long> ids = new ArrayList<>(list.size()); // 店铺id列表
+        Map<String, Distance> distanceMap = new HashMap<>(list.size()); // 店铺id到距离的映射
+        list.stream().skip(from).forEach(it -> { // 跳过前from个
+            String shopIdStr = it.getContent().getName();
+            ids.add(Long.valueOf(shopIdStr));
+            Distance distance = it.getDistance();
+            distanceMap.put(shopIdStr, distance);
+        });
+        // 根据id查询店铺
+        String idStr = StrUtil.join(",", ids);
+        List<Shop> shops = query().in("id", ids).last("ORDER BY FIELD(id," + idStr + ")").list(); // 保持顺序一致
+        for (Shop shop : shops) {
+            shop.setDistance(distanceMap.get(shop.getId().toString()).getValue());
+        }
+        return Result.ok(shops);
     }
 
 
--- src/test/java/com/mercemay/comment/CommentApplicationTests.java
+++ src/test/java/com/mercemay/comment/CommentApplicationTests.java
@@ -9,10 +9,19 @@ import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.mercemay.comment.utils.RedisConstants.SHOP_GEO_KEY;
 
 @SpringBootTest
 class CommentApplicationTests {
@@ -25,6 +34,8 @@ class CommentApplicationTests {
 
     @Resource
     private RedissonClient redissonClient;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     @Test
     void saveHotKeyToRedis() {
@@ -63,6 +74,28 @@ class CommentApplicationTests {
                 lock.unlock();
                 System.out.println("Lock released");
             }
+        }
+    }
+
+    @Test
+    void localShopData() {
+        List<Shop> shops = shopService.list(); // 获取所有店铺数据
+        Map<Long, List<Shop>> map = shops.stream()
+                .collect(Collectors.groupingBy(Shop::getTypeId)); // 按类型分组
+        // 分批写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            List<Shop> shopList = entry.getValue(); // 获取同类型的店铺列表
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(shopList.size());
+            // 把店铺位置写入Redis
+            for (Shop shop : shopList) {
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
 }
